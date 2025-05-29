0986783f49777772250e836f281d674aa9e50fea
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-05-28 20:47:35 -0600

    position absolute

--- src/presentation/screens/PositionScreen.tsx
+++ src/presentation/screens/PositionScreen.tsx
@@ -4,19 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={ styles.container }>
+      <View style={ styles.greenBox } />
       <View style={ styles.purpleBox } />
 
       <View style={ styles.orangeBox } />
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // hace que el ancho sea el del contenido
+    flex: 1, // hace que el ancho sea el del contenido 
+    // height: 250,
+    // width: 300,
     backgroundColor: '#28c4d9',
-    justifyContent: 'center',
-    alignItems: 'center'
+    // justifyContent: 'center',
+    // alignItems: 'center'
   },
   purpleBox: {
     width: 100,
@@ -24,14 +28,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856d6',
     borderWidth: 10,
     borderColor: 'white',
-    position: 'relative',
-    top: 30
+    position: 'absolute',
+    bottom: 0
+    // top: 30
   },
   orangeBox: {
     width: 100,
     height: 100,
     backgroundColor: '#f0a23b',
     borderWidth: 10,
-    borderColor: 'white'
+    borderColor: 'white',
+    position: 'absolute',
+    right: 0
+  },
+  greenBox: {
+    // width: 100,
+    // height: 100,
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+
+
+    // ESTAS PROPIEDADES SIRVEN PARA ABARCAR TODO EL CONTENIDO DEL PADRE
+    // position: 'absolute',
+    // bottom: 0,
+    // right: 0,
+    // top: 0,
+    // left: 0,
+
+    // LA SIGUIENTE ES LO MISMO QUE LAS PROPIEDADES ANTERIORES
+    ...StyleSheet.absoluteFillObject,
   },
 });
